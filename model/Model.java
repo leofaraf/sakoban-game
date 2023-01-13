7389f5ccace6c1addbe101dae0bd5300eb0bb7ae
leonfarafonov <117378906+leonfarafonov@users.noreply.github.com>
2023-01-13 18:36:21 +0300

    Added logic move

--- model/Model.java
+++ model/Model.java
@@ -11,6 +11,70 @@ public class Model {
     private int currentLevel = 1;
     private LevelLoader levelLoader;
 
+    public boolean checkWallCollision(CollisionObject gameObject, Direction direction) {
+        for (GameObject wall: getGameObjects().getWalls()) {
+            if (gameObject.isCollision(wall, direction)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public boolean checkBoxCollisionAndMoveIfAvailable(Direction direction) {
+        for (Box box : gameObjects.getBoxes()) {
+            if (gameObjects.getPlayer().isCollision(box, direction)) {
+                for (Box item : gameObjects.getBoxes()) {
+                    if (!box.equals(item)) {
+                        if (box.isCollision(item, direction)) {
+                            return true;
+                        }
+                    }
+                    if (checkWallCollision(box, direction)) {
+                        return true;
+                    }
+                }
+                int dx = direction == Direction.LEFT ? -FIELD_CELL_SIZE : (direction == Direction.RIGHT ? FIELD_CELL_SIZE : 0);
+                int dy = direction == Direction.UP ? -FIELD_CELL_SIZE : (direction == Direction.DOWN ? FIELD_CELL_SIZE : 0);
+                box.move(dx, dy);
+            }
+        }
+        return false;
+    }
+
+    public void checkCompletion() {
+        int numberOfHomes = gameObjects.getHomes().size();
+        int numberOfHomesWithBox = 0;
+
+        for (Home home : gameObjects.getHomes()) {
+            for (Box box : gameObjects.getBoxes()) {
+                if (box.getX() == home.getX() && box.getY() == home.getY()) {
+                    numberOfHomesWithBox++;
+                }
+            }
+        }
+
+        if (numberOfHomesWithBox == numberOfHomes) {
+            eventListener.levelCompleted(currentLevel);
+        }
+    }
+
+    public void move(Direction direction) {
+        if (checkWallCollision(gameObjects.getPlayer(), direction)) {
+            return;
+        }
+
+        if (checkBoxCollisionAndMoveIfAvailable(direction)) {
+            return;
+        }
+
+        int dx = direction == Direction.LEFT ? -FIELD_CELL_SIZE : (direction == Direction.RIGHT ? FIELD_CELL_SIZE : 0);
+        int dy = direction == Direction.UP ? -FIELD_CELL_SIZE : (direction == Direction.DOWN ? FIELD_CELL_SIZE : 0);
+        gameObjects.getPlayer().move(dx, dy);
+
+        checkCompletion();
+    }
+
+
     public Model() {
         try {
             levelLoader = new LevelLoader(Paths.get(getClass().getResource("../res/levels.txt").toURI()));
@@ -39,8 +103,5 @@ public class Model {
 
     public void setEventListener(EventListener eventListener) {
         this.eventListener = eventListener;
-    }
-
-    public void move(Direction direction) {
     }
 }

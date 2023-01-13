de7f52b6b710ce556968fa30c21b380b2d6146fe
leonfarafonov <117378906+leonfarafonov@users.noreply.github.com>
2023-01-13 17:50:04 +0300

    Update model

--- model/Model.java
+++ model/Model.java
@@ -2,9 +2,42 @@ package com.javarush.task.task34.task3410.model;
 
 import com.javarush.task.task34.task3410.controller.EventListener;
 
+import java.net.URISyntaxException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.logging.Level;
+
 public class Model {
     private EventListener eventListener;
+    private GameObjects gameObjects;
+    private int currentLevel = 1;
+    private LevelLoader levelLoader;
+
+    public Model() {
+        try {
+            levelLoader = new LevelLoader(Paths.get(getClass().getResource("../res/levels.txt").toURI()));
+        } catch (URISyntaxException e) {
+        }
+    }
+
     public static final int FIELD_CELL_SIZE = 20;
+
+    public GameObjects getGameObjects() {
+        return gameObjects;
+    }
+
+    public void restartLevel(int level) {
+        gameObjects = levelLoader.getLevel(level);
+    }
+
+    public void restart() {
+        restartLevel(currentLevel);
+    }
+
+    public void startNextLevel() {
+        currentLevel += 1;
+        restart();
+    }
 
     public void setEventListener(EventListener eventListener) {
         this.eventListener = eventListener;
